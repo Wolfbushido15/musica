e554413e7cc93bb4437cdc87c9db55dda184a13a
Wolfbushido15 <joaoguilherme20999@gmail.com>
2024-12-03 17:18:46 -0300

    Musica

---
+++ musica.py
@@ -0,0 +1,20 @@
+import youtube_dl # client para muitos portais de multimídia
+
+# baixa yt_url para o mesmo diretório no qual o script é executado
+def download_audio(yt_url):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([yt_url])
+
+def main():
+    yt_url = "https://www.youtube.com/watch?v=8OAPLk20epo"
+    download_audio(yt_url)
+
+main()
